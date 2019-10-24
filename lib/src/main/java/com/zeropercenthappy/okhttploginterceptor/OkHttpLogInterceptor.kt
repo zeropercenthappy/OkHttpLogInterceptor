727feced98d8dfc58679cd148001fd7343a813c9
zeropercenthappy <zeropercenthappy@foxmail.com>
2019-10-24 10:49:11 +0800

    init commit

---
+++ lib/src/main/java/com/zeropercenthappy/okhttploginterceptor/OkHttpLogInterceptor.kt
@@ -0,0 +1,111 @@
+package com.zeropercenthappy.okhttploginterceptor
+
+import android.text.TextUtils
+import android.util.Log
+import okhttp3.*
+import okio.Buffer
+import java.nio.charset.Charset
+import java.util.*
+import java.util.regex.Pattern
+
+class OkHttpLogInterceptor(private val logTag: String? = DEFAULT_LOG_TAG) : Interceptor {
+
+    companion object {
+        private const val DEFAULT_LOG_TAG = "OkHttp"
+        private const val PART_VALUE_REGEX = "(?<=name=\").*?(?=\")"
+        private val VALUE_PATTERN = Pattern.compile(PART_VALUE_REGEX)
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        log("===")
+
+        log("url:${request.url.toUrl()}")
+
+        log("method:${request.method}")
+
+        logHeader(request.headers)
+
+        logParameter(request.url)
+
+        logBody(request.body)
+
+        val response = chain.proceed(request)
+        logResponse(response)
+
+        log("===")
+
+        return response
+    }
+
+    private fun logHeader(headers: Headers) {
+        for (i in 0 until headers.size) {
+            log("header: ${headers.name(i)}=${headers.value(i)}")
+        }
+    }
+
+    private fun logParameter(httpUrl: HttpUrl) {
+        for (name in httpUrl.queryParameterNames) {
+            for (value in httpUrl.queryParameterValues(name)) {
+                log("url parameter: $name=$value")
+            }
+        }
+    }
+
+    private fun logBody(requestBody: RequestBody?) {
+        if (requestBody == null) {
+            return
+        }
+        when (requestBody) {
+            is FormBody -> {
+                for (i in 0 until requestBody.size) {
+                    log("form body: ${requestBody.name(i)}=${requestBody.value(i)}")
+                }
+            }
+            is MultipartBody -> {
+                for (part in requestBody.parts) {
+                    val size = part.headers?.size ?: 0
+                    for (i in 0 until size) {
+                        if (TextUtils.equals(part.headers?.name(i), "Content-Disposition")) {
+                            val matcher = VALUE_PATTERN.matcher(part.headers?.value(i))
+                            matcher.find()
+                            val key = matcher.group()
+                            if (TextUtils.equals(part.body.contentType()?.type, "text")) {
+                                val buffer = Buffer()
+                                part.body.writeTo(buffer)
+                                val value = buffer.readString(Charset.defaultCharset())
+                                log("multipart body: $key=$value")
+                            } else {
+                                log("multipart body: $key={binary},size=${part.body.contentLength() / 1024}KB")
+                            }
+                        }
+                    }
+                }
+            }
+            else -> {
+                if (TextUtils.equals(requestBody.contentType()?.subtype?.toLowerCase(Locale.US), "json")) {
+                    val buffer = Buffer()
+                    requestBody.writeTo(buffer)
+                    val value = buffer.readString(Charset.defaultCharset())
+                    log("json body: $value")
+                }
+            }
+        }
+    }
+
+    private fun logResponse(response: Response?) {
+        if (response == null) {
+            return
+        }
+        val body = response.body ?: return
+        if (TextUtils.equals(body.contentType()?.type, "application")) {
+            log("response: ${body.string()}")
+        } else {
+            log("response: ${body.contentType()?.type}/${body.contentType()?.subtype}, size=${body.contentLength() / 1024}KB")
+        }
+    }
+
+    private fun log(content: String) {
+        Log.i(logTag, content)
+    }
+}
